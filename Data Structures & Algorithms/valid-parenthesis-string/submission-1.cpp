97611fe2f761539d6bf73d252373837da8675766
Sharv Chopra <sharvchopra12@gmail.com>
2026-05-07 16:38:12 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        //Method-1: Greedy method:
+        // int mn = 0; // min possible no. of unmatched '('
+        // int mx = 0; // max possible no. of unmatched '('
+
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i] == '('){
+        //         mn++;
+        //         mx++;
+        //     }
+        //     else if(s[i] == ')'){
+        //         mn--;
+        //         mx--;
+        //     }
+        //     else{
+        //         mn--;
+        //         mx++;
+        //     }
+
+        //     if(mx<0){
+        //         return false;
+        //     }
+        //     if(mn<0){
+        //         mn = 0;
+        //     }
+        // }
+        // return mn == 0;
+
+        //Method-2;
+        //Stack -based approach
+        stack<char>opening_bracket,star_bracket;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                opening_bracket.push(i);
+            }
+            else if(s[i] == '*'){
+                star_bracket.push(i);
+            }
+            else{
+                if(opening_bracket.empty() && star_bracket.empty()){
+                    return false;
+                }
+                if(!opening_bracket.empty()){
+                    opening_bracket.pop();
+                }
+                if(!star_bracket.empty()){
+                    star_bracket.pop();
+                }
+            }
+        }
+
+        while(!opening_bracket.empty() && !star_bracket.empty()){
+            if(opening_bracket.top()>star_bracket.top()){
+                return false;
+            }
+            opening_bracket.pop();
+            star_bracket.pop();
+        }
+
+        return opening_bracket.empty();
+    }
+};
