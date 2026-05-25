f2b5d536cf82e2294158a0fb944a44e5eddaad61
Sharv Chopra <sharvchopra12@gmail.com>
2026-05-25 23:26:47 +0530

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+
+        const int min = -2147483648; 
+        const int max = 2147483647;
+
+        while(x!=0){
+            int digit = x%10;
+            x /=10;
+            
+            if(res>max/10 || (res == max/10 && digit>max%10))
+                return 0;
+            
+            if(res<min/10 || (res == min/10 && digit<min%10))
+                return 0;
+            
+            res = (res*10)+digit;
+        }
+        return res;
+    }
+};
